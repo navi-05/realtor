7cc9cca063bb49ac13d2825c8d2158f9e52d9cb4
navi-05 <naveenthatsmyname5@gmail.com>
2023-05-25 17:39:21 +0530

    removed add event listener

--- app/layout.js
+++ app/layout.js
@@ -4,18 +4,11 @@ import { ChakraProvider, Box } from '@chakra-ui/react'
 import Navbar from '@components/Navbar'
 import Footer from '@components/Footer'
 import NProgress from 'nprogress'
-import Router from 'next/router'
 
 import './globals.css'
 
 export default function RootLayout({ children }) {
   NProgress.configure({ showSpinner: false })
-
-  Router.events.on('routeChangeStart', () =>  NProgress.start());
-  Router.events.on('routeChangeComplete', () =>  NProgress.done());
-  Router.events.on('routeChangeError', () =>  NProgress.done());
-
-  addEventListener('navigate', (event) => {event.on('route')})
 
   return (
     <html lang="en">
